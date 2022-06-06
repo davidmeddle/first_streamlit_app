159cbd9ca00fe24bda919c942f33da9572d0edf6
Francisco Diaz <106951646+davidmeddle@users.noreply.github.com>
2022-06-06 17:49:54 +1000

    Update streamlit_app.py
    added coffee

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import streamlit
 streamlit.title('My partners new the healthy food')
 streamlit.header('Break fastmenu')
 streamlit.text ('Omega 3 and kale')
+streamlit.text ('blueberrys and mil')
+streamlit.text ('almond latte')
 
