369f70e7c0165a92cc4fee77400d4d572248a404
Francisco Diaz <106951646+davidmeddle@users.noreply.github.com>
2022-06-08 15:11:20 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,14 @@
-import streamlit
+Import streamlit
 
-streamlit.title('My partners new the healthy food')
-streamlit.header('🥣 Break fastmenu')
-streamlit.text ('🐔 Omega 3 and kale')
-streamlit.text ('blueberrys and mil')
-streamlit.text ('almond latte')
+streamlit.title ('My parents new healthy diner')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-#fruits_selected = streamlit.multiselect("Pick some fruits:"), list (my_fruit_list.index),['Avocado'])
-#fruits_selected = 
-streamlit.multiselect("Pick some fruits:"), list (my_fruit_list.index))
-fruits_to_show =  my_fruit_list.loc(fruits_selected)
-#streamlit.dataframe(fruits_to_show)
