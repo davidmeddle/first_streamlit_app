84e3ec3ad53640b1872fa6eabd3c0f17f1e9ba92
Francisco Diaz <106951646+davidmeddle@users.noreply.github.com>
2022-06-08 15:41:35 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,6 @@ import requests
 
 
 fruitvice_response = requests.get ("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruitvice_response.jason())
+streamlit.text(fruitvice_response.json())
 
 
